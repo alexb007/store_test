d28ac948911320f853387c0ce195c34fbab6946b
Bakhrom <alex960126@gmail.com>
2019-08-03 03:16:50 +0500

    fix queues

--- StoreTest/celery.py
+++ StoreTest/celery.py
@@ -5,5 +5,6 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StoreTest.settings')
 
 app = Celery('StoreTest')
+app.conf.task_routes = {'store.tasks.*': {'queue': 'store'}}
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
