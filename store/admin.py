3b0aae741ecd3ab37f694bd901205f75009a6d45
Bakhrom <alex960126@gmail.com>
2019-08-03 03:17:09 +0500

    fix ordering

--- store/admin.py
+++ store/admin.py
@@ -9,6 +9,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display_links = ('name', 'status')
     list_filter = ('status',)
     search_fields = ('name',)
+    ordering = ('-created_at', 'name', 'status')
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
